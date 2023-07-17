dd53599bce53f92b5e141ad2f06172f42c50b2ca
OIgboerika <o.igboerika@alustudent.com>
2023-07-17 13:20:32 -0700

    task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return "{:d}".format(my_list[idx])
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
