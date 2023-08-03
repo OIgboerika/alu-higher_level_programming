10e34e778b226bd4efe2d288328dcd32d715f2f8
OIgboerika <o.igboerika@alustudent.com>
2023-08-03 03:40:42 -0700

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- creating a user and granting privileges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES *.* TO user_0d_1@localhost;
+-- a script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
