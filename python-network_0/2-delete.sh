49dae614e075ffd3763f206d77ef0d08f93fa76c
OIgboerika <o.igboerika@alustudent.com>
2023-08-03 22:45:40 -0700

    task 2

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a DELETE request
-curl -X DELETE -sSL "$1"
+curl -X DELETE -sSL "$1"; [ "$(curl -X DELETE -sSL "$1")" = "DELETE method accepted" ] && echo "Test Passed: DELETE method accepted" || echo "Test Failed: Response does not match expected output"
+
