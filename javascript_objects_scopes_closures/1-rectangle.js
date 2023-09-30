77c2529d840f9596011c258d5d82f82618ac7cf0
OIgboerika <o.igboerika@alustudent.com>
2023-09-30 14:32:23 -0700

    task 1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor(w, h) { 
     // Check if either w or h is not a positive number, set both to 0 in that case
     if (w <= 0 || h <= 0) {
       this.width = 3;
